528f79ff48bdba0b37b2dbb8bd191f830ae3dae3
Dhananjay Singh <dhananjay1204@gmail.com>
2018-09-14 15:07:55 +0530

    Simple angular dialogflow client

---
+++ simple-angular-dialogflow/src/app/chat/models/message.ts
@@ -0,0 +1,10 @@
+export class Message {
+    content: string;
+    sentBy: string;
+    avatar: string;
+    constructor(content: string, sentBy: string, avatar: string) {
+        this.content = content;
+        this.sentBy = sentBy;
+        this.avatar = avatar;
+    }
+}
